64e7209a9619b4a0efeb8de692ac36269b96df63
Rajendra Pancholi <rpancholi522@gmail.com>
2024-08-20 12:13:16 +0530

    Pattern Type stars 2.3 : piramid pattern.

---
+++ starspattern03.cpp
@@ -0,0 +1,34 @@
+// Pattern Type stars 2.3 : piramid pattern.
+//    *
+//   * *
+//  * * *
+// * * * *
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    int row = 1;
+    while (row <= n)
+    {
+        // print space
+        int space = n - row;
+        while (space)
+        {
+            cout << " ";
+            space = space - 1;
+        }
+        // print star
+        int col = 1;
+        while (col <= row)
+        {
+            cout << "* ";
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
