6829f0514a107404404ce90b9161e2913bb546b5
Rajendra Pancholi <rpancholi522@gmail.com>
2024-08-20 12:29:31 +0530

    Pattern Type Alphabets 3.2 : Square alphabets(increase column whith respect to previous row and then increment each column) pattern.

---
+++ alphapattern03.cpp
@@ -0,0 +1,26 @@
+// Pattern Type Alphabets 3.2 : Square alphabets(increase column whith respect to previous row and then increment each column) pattern.
+// A B C
+// B C D
+// C D E
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 3;
+    int i = 1;
+    char ch = 'A';
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            // type casting int to char.
+            char chh = ch + i + j - 2;
+            cout << chh << " ";
+            j++;
+        }
+        i++;
+        cout << endl;
+    }
+    return 0;
+}
